86cb23eacdbd6ffa7aa1b678bd3a59cda9dd0fd2
Nelson Ramirez <xhyskoax@gmail.com>
2025-11-04 18:14:40 -0300

    M2 - Lección ° 4, Segundo avance ejercicio de 04-11-2025

---
+++ m2/leccion_4_2/assets/js/script.js
@@ -0,0 +1,4 @@
+// HABILITAR TOOLTIPS
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
